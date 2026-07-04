1d6b47eec051eef9a2a14c763bed34eaa237ac02
Mukesh Aryal <mukeshnuwakot@gmail.com>
2026-07-04 22:15:01 +0545

    Learned about modules and shebang

--- Modules/app.js
+++ Modules/app.js
@@ -1,6 +1,8 @@
 // Now, we understand some basic things to use the module system of NodeJS
 // For CommonJS modules, we use the require function
 
+a = 20;
+
 const sum = require('./sum');
 
 console.log(sum(1, 2, 3, 4, 5));
---
+++ Shebang/app.js
@@ -0,0 +1,3 @@
+#! /usr/bin/env node
+
+console.log("Node is running me!");
